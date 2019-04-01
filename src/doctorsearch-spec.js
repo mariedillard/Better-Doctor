03ed6efe4ac1411b639572cdc9060d472e4a3393
Guest User <Guest@epicodus-31.local>
2019-04-01 15:20:26 -0700

    adds error messages, updates and clears information correctly. notes bug with incorrectly returning phone numbers.

--- src/doctorsearch-spec.js
+++ src/doctorsearch-spec.js
@@ -9,16 +9,22 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showCount').text(`${body.meta.count}`);
-      for (let i = 0; i < body.meta.count; i++) {
-        $('.showNames').append(`
-        <br>Practice Name: ${body.data[i].practices[0].name}
-        <br>Practitioner: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name} ${body.data[i].profile.title} 
-        <br>Street Address: ${body.data[i].practices[0].visit_address.city} ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.zip} & 
-        <br>Phone number: ${body.data[i].practices[0].phones.number} & 
-        <br>Website: ${body.data[i].practices[0].website} & 
-        <br>Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}
-        `); 
+      $( ".showCount" ).empty();
+      $( ".showNames" ).empty();
+      if (body.meta.count <= 0) {
+        $('.showNames').text(`none available`);
+      } else {
+        for (let i = 0; i < body.meta.count; i++) {
+          $('.showCount').text(`Doctors Available: ${body.meta.count}`);
+          $('.showNames').append(`
+          <br>Practice Name: ${body.data[i].practices[0].name}
+          <br>Practitioner: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name} ${body.data[i].profile.title} 
+          <br>Street Address: ${body.data[i].practices[0].visit_address.city} ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.zip} & 
+          <br>Phone number: ${body.data[i].practices[0].phones.number} & 
+          <br>Website: ${body.data[i].practices[0].website} & 
+          <br>Accepting New Patients: ${body.data[i].practices[0].accepts_new_patients}
+          `); 
+        }
       }
     }, function(error) {
       $('.showErrors').text(`There was an error processsing your request: ${error.message}`);
